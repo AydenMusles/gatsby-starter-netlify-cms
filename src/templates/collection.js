d797b8e891181baacc7885c92f3022291332f535
AydenMusles <41048702+AydenMusles@users.noreply.github.com>
2020-01-24 21:40:32 -0500

    Create collection.js

---
+++ src/templates/collection.js
@@ -0,0 +1,60 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import Container from "../styles/Container"
+import Collection from "../components/Collection"
+
+const CollectionTemplate = ({data}) => {
+    const collection = data.shopifyCollection
+
+    return (
+        <Container>
+            <Collection collection={collection}/>
+        </Container>
+    )
+}
+
+CollectionTemplate.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    query($shopifyId: String!) {
+        shopifyCollection(shopifyId: {eq: $shopifyId}) {
+            shopifyId
+            handle
+            title
+            image {
+                localFile {
+                    childImageSharp {
+                        fluid(maxWidth: 1000) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+            products {
+                shopifyId
+                title
+                handle
+                productType
+                priceRange {
+                    minVariantPrice {
+                        amount
+                    }
+                }
+                images {
+                    localFile {
+                        childImageSharp {
+                            fluid(maxWidth: 300) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default CollectionTemplate
