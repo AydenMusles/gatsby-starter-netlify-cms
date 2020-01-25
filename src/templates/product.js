ec9f58fbd21815f4e37c47dee613390ff6760ca8
AydenMusles <41048702+AydenMusles@users.noreply.github.com>
2020-01-24 21:40:00 -0500

    Create product.js

---
+++ src/templates/product.js
@@ -0,0 +1,69 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import Container from "../styles/Container"
+import Product from "../components/Product/Product"
+
+const ProductTemplate = ({data}) => {
+    const product = data.shopifyProduct
+
+    return (
+        <Container>
+            <Product product={product}/>
+        </Container>
+    )
+}
+
+ProductTemplate.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    query($shopifyId: String!) {
+        shopifyProduct(shopifyId: {eq: $shopifyId}) {
+            shopifyId
+            handle
+            title
+            productType
+            tags
+            descriptionHtml
+            availableForSale
+            images {
+                localFile {
+                    childImageSharp {
+                        fluid(maxWidth: 650) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+            priceRange {
+                minVariantPrice {
+                    amount
+                }
+            }
+            options {
+                name
+                values
+            }
+            variants {
+                shopifyId
+                selectedOptions {
+                    name
+                    value
+                }
+                image {
+                    localFile {
+                        childImageSharp {
+                            fluid(maxWidth: 650) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default ProductTemplate
